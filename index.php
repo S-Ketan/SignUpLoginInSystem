5542cd62386fbcf6a11ab8935fb1a0f9fea673bd
S-Ketan <ketan10923@gmail.com>
2025-03-23 15:52:06 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Signup</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+    <div
+        style="display: flex; justify-content: space-around; flex-direction: column; align-items: center; height: 100vh; gap: 20px;">
+        <div>
+
+            <h3>Login</h3>
+
+            <form action="includes/login.inc.php" method="post">
+                <input type="text" name="username" placeholder="Username">
+                <input type="password" name="pwd" placeholder="Password">
+                <button>Login</button>
+            </form>
+        </div>
+        <div>
+
+
+            <h3>Signup</h3>
+
+            <form action="includes/login.inc.php" method="post">
+                <input type="text" name="username" placeholder="Username">
+                <input type="password" name="pwd" placeholder="Password">
+                <input type="text" name="email" placeholder="Email">
+                <button>Signup</button>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
